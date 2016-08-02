5387dd6296fff7b296afedb72a54983118e59fee
chetan-techwave <chetan-padasalgi@techwave.net>
2016-08-02 17:37:00 +0530

    added content

--- test.php
+++ test.php
@@ -6,3 +6,8 @@ Why do we use it?
 
 It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
 
+
+
+
+During the regular IT Security Audit, it has been observed through our proxy/firewall/security systems that some of the associates are found indulging in accessing, downloading and exchanging prohibited material through our Corporate Network. It is hereby suggested to refrain from such kind of activities to avoid disciplinary actions. Associates must ensure the Confidentiality, Privacy and Commercial-in-Confidence Standards, Practices and Requirements are applied to the use of IT equipment and storage, retrieval, access/dissemination of information.
+
